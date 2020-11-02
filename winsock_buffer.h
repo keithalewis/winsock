b6f4b5b6e9dba9c969dd2fe95f74140164c0dca8
Keith A. Lewis <keithalewis@live.com>
2020-11-02 02:25:23 -0500

    sync

--- winsock_buffer.h
+++ winsock_buffer.h
@@ -1,6 +1,7 @@
 // buffer.h - buffer using char array, vector, iostream
 #pragma once
 #include <cstring>
+#include <stdexcept>
 #include <Windows.h>
 
 // not really winsock specific!!!
@@ -47,8 +48,9 @@ namespace winsock {
 	struct buffer {
 		T* buf;
 		int len;
-		int off;
-
+	private:
+		int off; // can only be reset to 0
+	public:
 		buffer(T* buf, size_t len)
 			: buf(buf), len(static_cast<int>(len)), off(0)
 		{ }
@@ -75,10 +77,10 @@ namespace winsock {
 			return buf[i];
 		}
 
-		// set offset
-		void offset(int o = 0)
+		// reset offset
+		void reset()
 		{
-			off = o;
+			off = 0;
 		}
 
 		// buffer<B> b; while (buf = b(n)) { send(buf.buf, buf.len); }
@@ -121,13 +123,9 @@ namespace winsock {
 		obuffer(char* buf, int len = 0)
 			: buffer<char>(buf, len ? len : static_cast<int>(strlen(buf)))
 		{ }
-		void length(int l)
-		{
-			len = l;
-		}
 	};
 
-	// buffer backed by anonymous file
+	// buffer backed by anonymous memory mapped file
 	class iobuffer : public buffer<char>
 	{
 		handle h;
